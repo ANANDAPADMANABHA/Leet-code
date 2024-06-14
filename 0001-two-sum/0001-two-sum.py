f3aab30bfc5fd7a89006012235e5df9c5b7fae1b
Anantha padmanabhan <82267205+ANANDAPADMANABHA@users.noreply.github.com>
2024-06-14 21:24:38 +0530

    Time: 34 ms (89.15%), Space: 12.6 MB (42.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,7 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i,j]
+        prviousmap = {}
+        for i in range(len(nums)):
+            dif = target - nums[i]
+            if dif in prviousmap:
+                return [i,prviousmap[dif]]
+            prviousmap[nums[i]] = i
