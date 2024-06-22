93937f892892056879646cc54530bad008cab222
Anantha padmanabhan <82267205+ANANDAPADMANABHA@users.noreply.github.com>
2024-06-22 14:00:44 +0530

    Time: 22 ms (48.00%), Space: 11.8 MB (68.28%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        def backtrack(start):
+            if start == len(nums):
+                result.append(nums[:]) 
+                return
+
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+
+        result = []
+        backtrack(0)
+        return result
