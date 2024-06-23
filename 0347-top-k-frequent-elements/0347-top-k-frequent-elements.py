2271b149a8f6caba562b837df0c7c0d0ff1005c3
Anantha padmanabhan <82267205+ANANDAPADMANABHA@users.noreply.github.com>
2024-06-23 22:54:53 +0530

    Time: 86 ms (20.19%), Space: 19 MB (25.59%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,22 +5,21 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        # Step 1: Count the frequency of each element
-        count = Counter(nums)
-
-        # Step 2: Create an array of empty lists to act as buckets
-        # The length of the bucket array should be len(nums) + 1 because the maximum frequency any number can 
-        freq = [[] for _ in range(len(nums) + 1)]
-
-        # Step 3: Fill the buckets
-        for num, cnt in count.items():
-            freq[cnt].append(num)
-
-        # Step 4: Gather the top k frequent elements from the buckets
-        res = []
-        for i in range(len(freq) - 1, 0, -1):  # Start from the end of the list
-            for num in freq[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+        counter = {}
+        for i in nums:
+            counter[i] = counter.get(i,0)+1
+            
+        nums_list = [[] for _ in range(len(nums)+1)]
+        
+        for num,count in counter.items():
+            nums_list[count].append(num)
+            
+        ans = []
+        
+        for i in range(len(nums),0,-1):
+            for j in nums_list[i]:
+                ans.append(j)
+                if len(ans) == k:
+                    return ans
+            
                 
