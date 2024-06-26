18fbdad7822573667139cbefe566e2735ef5df53
Anantha padmanabhan <82267205+ANANDAPADMANABHA@users.noreply.github.com>
2024-06-26 15:46:58 +0530

    Time: 84 ms (52.90%), Space: 12.6 MB (93.56%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -4,13 +4,36 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        counter = {}
-        for i in nums:
-            counter[i] = counter.get(i,0)+1
-        
-        output = []
-        for char,occurance in counter.items():
-            if occurance > len(nums)/3:
-                output.append(char)
-                
-        return output
+
+        # Step 1: Find potential candidates
+        candidate1, candidate2, count1, count2 = None, None, 0, 0
+
+        for num in nums:
+            if candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+
+        # Step 2: Verify candidates
+        result = []
+        count1, count2 = 0, 0
+
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+
+        if count1 > len(nums) // 3:
+            result.append(candidate1)
+        if count2 > len(nums) // 3:
+            result.append(candidate2)
+
+        return result
