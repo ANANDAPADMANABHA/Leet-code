013019856444ac07eb8f996f41fa7eaaca1a6a4e
Anantha padmanabhan <82267205+ANANDAPADMANABHA@users.noreply.github.com>
2024-06-29 17:00:38 +0530

    Time: 25 ms (79.53%), Space: 13.6 MB (34.13%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        normalized_s = ''.join([ch.lower() for ch in s if ch.isalnum()])
-    
-        # Step 2: Use two pointers to check palindrome
-        if normalized_s == normalized_s[::-1]:
-            return True
-        return False
+        alpha_num = []
+        for i in s:
+            if i.isalnum():
+                alpha_num.append(i)
+        ans = ''.join(alpha_num)
+        return ans.lower() == ans[::-1].lower()
