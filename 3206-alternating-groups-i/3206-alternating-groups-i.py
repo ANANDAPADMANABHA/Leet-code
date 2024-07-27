937f92b2ba7a79a751e4aa6f8296c5d2d2d1b7d2
Anantha padmanabhan <82267205+ANANDAPADMANABHA@users.noreply.github.com>
2024-07-27 17:57:34 +0530

    Time: 39 ms (13.50%), Space: 11.6 MB (37.58%) - LeetHub

--- 3206-alternating-groups-i/3206-alternating-groups-i.py
+++ 3206-alternating-groups-i/3206-alternating-groups-i.py
@@ -4,12 +4,12 @@ class Solution(object):
         :type colors: List[int]
         :rtype: int
         """
-        colors+=colors[:2]
+        colors += colors[:2]
         output = 0
         len_col = len(colors)
         for i in range(len_col-2):
             block = colors[i:i+3]
-            if block[0] == block[2] and block[1] != block[0]:
-                output += 1
+            if block == [1,0,1] or block == [0,1,0]:
+                output+=1
                 
         return output
