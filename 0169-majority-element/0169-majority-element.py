e7d5a67e3e682b2b9abf0f9e5d8e95197316532d
Anantha padmanabhan <82267205+ANANDAPADMANABHA@users.noreply.github.com>
2024-09-05 22:38:00 +0530

    Time: 164 ms (83.94%), Space: 18 MB (81.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,14 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        counter = {}
-        for i in nums:
-            counter[i] = counter.get(i,0)+1
-            
-        for char,occurance in counter.items():
-            if occurance > len(nums)/2:
-                return char
-            
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
