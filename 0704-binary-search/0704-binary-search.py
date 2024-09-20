1f65b92a351af9922b08bf9ebab7294b9e1f7a9f
Anantha padmanabhan <82267205+ANANDAPADMANABHA@users.noreply.github.com>
2024-09-20 09:07:07 +0530

    Time: 190 ms (62.02%), Space: 18 MB (69.70%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,14 +3,14 @@ class Solution:
         high = len(nums)-1
         low  = 0
         while high >= low:
-            mid = high+low//2
+            mid = (high+low)//2
             
             if nums[mid] == target:
                 return  mid
             
             elif nums[mid] > target:
-                high = mid - 1
+                high = mid -1
             else:
-                low = mid + 1
+                low = mid +1
                 
         return -1
