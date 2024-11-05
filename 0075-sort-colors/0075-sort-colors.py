a74078a6a60d32b723dda222bcfa0e1a3980089c
Anantha padmanabhan <82267205+ANANDAPADMANABHA@users.noreply.github.com>
2024-11-05 15:47:19 +0530

    Time: 0 ms (100.00%), Space: 11.6 MB (61.85%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,11 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        low, mid, high = 0, 0, len(nums) - 1
         
-        n = len(nums)
-        for i in range(n):
-            for j in range(n-1):
-                if nums[j] > nums[j+1]:
-                    nums[j],nums[j+1] = nums[j+1],nums[j]
-                    
-        return nums
+        while mid <= high:
+            if nums[mid] == 0:
+                # Swap nums[low] and nums[mid] and move both pointers
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                # If it's 1, just move the mid pointer
+                mid += 1
+            else:
+                # Swap nums[mid] and nums[high] and move high pointer
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
